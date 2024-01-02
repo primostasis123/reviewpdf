0249c387d03deb9808a02672f076376a99647b74
primostasis123 <editomoncada123@gmail.com>
2024-01-02 19:05:01 +0800

    remove test route
    removing test route

--- trpc/index.ts
+++ trpc/index.ts
@@ -1,10 +1,6 @@
 import { privateProcedure, publicProcedure, router } from './trpc'
 import { db } from '@/lib/db'
 export const appRouter = router({
-    test: publicProcedure.query(() => {
-        return 3
-    }),
-
     getUserFiles: privateProcedure.query(async ({ctx}) => {
         const { userId } = ctx
         return await db.file.findMany({
