a1438ceaf0509884db200f1c5840a03bda633a04
primostasis123 <editomoncada123@gmail.com>
2024-01-06 03:04:40 +0800

    component
    adding ChatWrapper component PdfRendererComponent

--- app/dashboard/[fileid]/page.tsx
+++ app/dashboard/[fileid]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/lib/db";
 import { auth } from "@/lib/auth";
 import { notFound } from "next/navigation";
+import { PdfRenderer } from "@/components/PdfRenderer";
+import { ChatWrapper } from "@/components/ChatWrapper";
 interface PageProps {
   params: {
     fileid: string;
@@ -22,5 +24,21 @@ export default async function page({ params }: PageProps) {
     notFound();
   }
 
-  return <div>{fileid}</div>;
+  return (
+    <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
+      <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
+        {/* left side */}
+        <div className="flex-1 xl:flex">
+          <div className="px-6 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PdfRenderer/>
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+            <ChatWrapper/>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -3,9 +3,17 @@
 import { useState } from "react";
 import { Dialog,DialogContent, DialogTrigger } from "./ui/dialog";
 import { Button } from "./ui/button";
+import Dropzone from "react-dropzone";
+
 export const UploadButton = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
-
+  const UploadDropZone = () => {
+    return <Dropzone multiple={false}>
+      {({getRootProps, getInputProps, acceptedFiles}) => (
+        <div {...getRootProps()} className="border h-64 m-4 border-dashed border-gray-300 rounded-lg"></div>
+      )}
+    </Dropzone>
+  }
   return (
     <Dialog
       open={isOpen}
@@ -18,7 +26,9 @@ export const UploadButton = () => {
       <DialogTrigger asChild onClick={() => setIsOpen(true)}>
         <Button>Upload PDF</Button>
       </DialogTrigger>
-      <DialogContent>example content</DialogContent>
+      <DialogContent>
+        <UploadDropZone/>
+      </DialogContent>
     </Dialog>
   );
 };
