39a75429d7fc5156fe2c9279ab630a3f4aa43ecd
primostasis123 <editomoncada123@gmail.com>
2024-01-08 01:52:51 +0800

    PdfRenderer Wrapper
    finalize pdf view
    add zoomin
    add fullscreen

--- app/dashboard/[fileid]/page.tsx
+++ app/dashboard/[fileid]/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/lib/db";
 import { auth } from "@/lib/auth";
 import { notFound } from "next/navigation";
-import { PdfRenderer } from "@/components/PdfRenderer";
+import PdfRenderer from "@/components/PdfRenderer";
 import { ChatWrapper } from "@/components/ChatWrapper";
 interface PageProps {
   params: {
@@ -27,16 +27,16 @@ export default async function page({ params }: PageProps) {
   return (
     <div className="flex-1 justify-between flex flex-col h-[calc(100vh-3.5rem)]">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
-        {/* left side */}
+        {/* Left sidebar & main wrapper */}
         <div className="flex-1 xl:flex">
-          <div className="px-6 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
-            <PdfRenderer url={file.url}/>
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* Main area */}
+            <PdfRenderer url={file.url} />
           </div>
         </div>
 
-        {/* right side */}
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-            <ChatWrapper/>
+          <ChatWrapper />
         </div>
       </div>
     </div>
