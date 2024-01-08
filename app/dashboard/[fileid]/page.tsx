a114168839c99787a9933837ebaeb4e515908d8f
primostasis123 <editomoncada123@gmail.com>
2024-01-09 02:54:53 +0800

    improve ChatWrapper
    add ChatInput component
    add Message component
    add trpc router getFileUploadStatus

--- app/dashboard/[fileid]/page.tsx
+++ app/dashboard/[fileid]/page.tsx
@@ -1,8 +1,9 @@
+
 import { db } from "@/lib/db";
 import { auth } from "@/lib/auth";
 import { notFound } from "next/navigation";
 import PdfRenderer from "@/components/PdfRenderer";
-import { ChatWrapper } from "@/components/ChatWrapper";
+import ChatWrapper from "@/components/chat/ChatWrapper";
 interface PageProps {
   params: {
     fileid: string;
@@ -36,7 +37,7 @@ export default async function page({ params }: PageProps) {
         </div>
 
         <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
-          <ChatWrapper />
+          <ChatWrapper fileId={file.id} />
         </div>
       </div>
     </div>
