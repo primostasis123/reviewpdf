97f594f64dda9969b7819a12a7d88c3b068c5bf4
primostasis123 <editomoncada123@gmail.com>
2024-01-26 21:57:54 +0800

    pricing page
    create pricing page

---
+++ app/pricing/page.tsx
@@ -0,0 +1,214 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { cn } from "@/lib/utils";
+import { Check, HelpCircle, Minus } from "lucide-react";
+
+export default function page() {
+  const pricingItems = [
+    {
+      plan: "Basic",
+      tagline: "For small pdf files.",
+      price: 9,
+      quota: 20,
+      features: [
+        {
+          text: "16MB file size limit",
+          footnote: "The maximum file size of a single PDF file.",
+        },
+        {
+          text: (
+            <a
+              href="https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them"
+              target="_blank"
+            >
+              <span className="underline text-sky-400">
+                4,096 tokens per response
+              </span>
+            </a>
+          ),
+          footnote: "Tokens can be thought of as pieces of words.",
+        },
+        {
+          text: "Mobile-friendly interface",
+        },
+        {
+          text: "Higher-quality responses",
+          footnote: "Better algorithmic responses for enhanced content quality",
+        },
+        {
+          text: "Email support",
+          negative: true,
+        },
+      ],
+    },
+    {
+      plan: "Pro",
+      tagline: "For larger pdf files.",
+      price: 12,
+      quota: 50,
+      features: [
+        {
+          text: "32MB file size limit",
+          footnote: "The maximum file size of a single PDF file.",
+        },
+        {
+          text: (
+            <a
+              href="https://help.openai.com/en/articles/4936856-what-are-tokens-and-how-to-count-them"
+              target="_blank"
+            >
+              <span className="underline text-sky-400">
+                16,385 tokens per response
+              </span>
+            </a>
+          ),
+          footnote: "Tokens can be thought of as pieces of words.",
+        },
+        {
+          text: "Mobile-friendly interface",
+        },
+        {
+          text: "Higher-quality responses",
+          footnote: "Better algorithmic responses for enhanced content quality",
+        },
+        {
+          text: "Email support",
+          negative: false,
+        },
+      ],
+    },
+  ];
+  return (
+    <>
+      <MaxWidthWrapper className="mb-8 mt-24 text-center max-w-5xl">
+        <div className="mx-auto mb-10 sm:max-w-lg">
+          <h1 className="text-6xl font-bold sm:text-7xl">Pricing</h1>
+          <p className="mt-5 text-gray-600 sm:text-lg">
+            Effortlessly distill information from documents, provide instant
+            answers to questions, and seamlessly comprehend research with the
+            power of AI
+          </p>
+        </div>
+
+        <div className="pt-12 grid grid-cols-1 gap-10 lg:grid-cols-2">
+          <TooltipProvider>
+            {pricingItems.map(({ plan, tagline, price, quota, features }) => {
+              return (
+                <div
+                  key={plan}
+                  className={cn("relative rounded-2xl bg-white shadow-lg", {
+                    "border-2 border-blue-600 shadow-blue-200": plan === "Pro",
+                    "border border-gray-200": plan !== "Pro",
+                  })}
+                >
+                  <div className="absolute -top-5 left-0 right-0 mx-auto w-32 rounded-full bg-gradient-to-r from-blue-600 to-cyan-600 px-3 py-2 text-sm font-medium text-white">
+                    Upgrade now
+                  </div>
+
+                  <div className="p-5">
+                    <h3 className="my-3 text-center font-display text-3xl font-bold">
+                      {plan}
+                    </h3>
+                    <p className="text-gray-500">{tagline}</p>
+                    <p className="my-5 font-display text-6xl font-semibold">
+                      ${price}
+                    </p>
+                    <p className="text-gray-500">per month</p>
+                  </div>
+
+                  <div className="flex h-20 items-center justify-center border-b border-t border-gray-200 bg-gray-50">
+                    <div className="flex items-center space-x-1">
+                      <p>{quota.toLocaleString()} PDFs/mo included</p>
+
+                      <Tooltip delayDuration={300}>
+                        <TooltipTrigger className="cursor-default ml-1.5">
+                          <HelpCircle className="h-4 w-4 text-zinc-500" />
+                        </TooltipTrigger>
+                        <TooltipContent className="w-80 p-2">
+                          How many PDFs you can upload per month.
+                        </TooltipContent>
+                      </Tooltip>
+                    </div>
+                  </div>
+
+                  <ul className="my-10 space-y-5 px-8">
+                    {features.map(({ text, footnote, negative }, index) => (
+                      <li key={index} className="flex space-x-5">
+                        <div className="flex-shrink-0">
+                          {negative ? (
+                            <Minus className="h-6 w-6 text-gray-300" />
+                          ) : (
+                            <Check className="h-6 w-6 text-blue-500" />
+                          )}
+                        </div>
+                        {footnote ? (
+                          <div className="flex items-center space-x-1">
+                            <p
+                              className={cn("text-gray-600", {
+                                "text-gray-400": negative,
+                              })}
+                            >
+                              {text}
+                            </p>
+                            <Tooltip delayDuration={300}>
+                              <TooltipTrigger className="cursor-default ml-1.5">
+                                <HelpCircle className="h-4 w-4 text-zinc-500" />
+                              </TooltipTrigger>
+                              <TooltipContent className="w-80 p-2">
+                                {footnote}
+                              </TooltipContent>
+                            </Tooltip>
+                          </div>
+                        ) : (
+                          <p
+                            className={cn("text-gray-600", {
+                              "text-gray-400": negative,
+                            })}
+                          >
+                            {text}
+                          </p>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                  <div className="border-t border-gray-200" />
+                  <div className="p-5">
+                    {/* {plan === 'Free' ? (
+                        <Link
+                          href={
+                            user ? '/dashboard' : '/sign-in'
+                          }
+                          className={buttonVariants({
+                            className: 'w-full',
+                            variant: 'secondary',
+                          })}>
+                          {user ? 'Upgrade now' : 'Sign up'}
+                          <ArrowRight className='h-5 w-5 ml-1.5' />
+                        </Link>
+                      ) : user ? (
+                        <UpgradeButton />
+                      ) : (
+                        <Link
+                          href='/sign-in'
+                          className={buttonVariants({
+                            className: 'w-full',
+                          })}>
+                          {user ? 'Upgrade now' : 'Sign up'}
+                          <ArrowRight className='h-5 w-5 ml-1.5' />
+                        </Link>
+                      )} */}
+                  </div>
+                </div>
+              );
+            })}
+          </TooltipProvider>
+        </div>
+      </MaxWidthWrapper>
+    </>
+  );
+}
