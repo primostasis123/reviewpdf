a5bbe71f7e079b738aab34eacac9acda4185608d
primostasis123 <editomoncada123@gmail.com>
2024-02-04 20:47:28 +0800

    clean dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,27 +1,8 @@
-import { auth } from "@/lib/auth"
 import { Dashboard } from "@/components/Dashboard";
 import { getUserSubscriptionPlan } from "@/lib/paypal";
 export default async function page() {
-
-
-  const session = await auth();
-
-  const test = await getUserSubscriptionPlan()
-
-  console.log(test)
-
-  // const response2 = await fetch(`https://api.sandbox.paypal.com/v1/billing/subscriptions/${subscriptionId}/cancel`, {
-  //   method: 'POST',
-  //   headers: {
-  //     'Content-Type': 'application/json',
-  //     'Authorization': `Bearer ${accessToken}`
-  //   }
-  // });
-
-  // console.log(response2);
-  
-
+  const subscriptionPlan  = await getUserSubscriptionPlan()
   return (
-    <Dashboard/>
+    <Dashboard  subscriptionPlan={subscriptionPlan}/>
   );
 }
