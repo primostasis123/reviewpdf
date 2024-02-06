397d7dedf0409a332d6547fd74d4328004c70f27
primostasis123 <editomoncada123@gmail.com>
2024-02-07 07:43:02 +0800

    loading login and register
    add loading state when click login or reigster

--- components/Login.tsx
+++ components/Login.tsx
@@ -7,119 +7,156 @@ import Link from "next/link";
 import { signIn } from "next-auth/react";
 import { useState } from "react";
 import { useSearchParams } from "next/navigation";
+import { useToast } from "./ui/use-toast";
+import { Loader2 } from "lucide-react";
 
 const Login = () => {
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const searchParams = useSearchParams();
-    const error = searchParams.get("error");
-  
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const searchParams = useSearchParams();
+  const error = searchParams.get("error");
+  const [isCredentials, setIsCredentials] = useState<boolean>(false);
+  const [isGoogle, setIsGoogle] = useState<boolean>(false);
+  const { toast } = useToast();
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      setIsCredentials(true);
       const res = await signIn("credentials", {
         email,
         password,
       });
-    };
-    return (
-      <div>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Icons.logo className="mx-auto h-10 w-auto" />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit}>
-              <Button
-                type="button"
-                className="bg-black w-full"
-                onClick={() => {
-                  signIn("google");
-                }}
-              >
-                <Icons.google className="mr-2 h-4 w-4" />
-                Continue with Google
-              </Button>
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t"></span>
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="grainy px-2 text-muted-foreground font-semibold">
-                    Or continue with
-                  </span>
-                </div>
-              </div>
-              <div>
-                <Label htmlFor="email"> Email address</Label>
-                <div className="mt-2">
-                  <Input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    required
-                    className="block text-sm font-medium leading-6"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <Label htmlFor="password"> Password</Label>
-                  <div className="text-sm">
-                    <a
-                      href="#"
-                      className="font-semibold text-blue-600 hover:text-blue-500"
-                    >
-                      Forgot password?
-                    </a>
-                  </div>
-                </div>
-                <div className="mt-2">
-                  <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-              </div>
-              {error ? (
-                <div>
-                  <div className="flex items-center justify-between">
-                    <Label className="text-red-600">Your email or password were incorrect.</Label>
-                  </div>
-                </div>
+    } catch (error) {
+      toast({
+        title: "Error",
+        description:
+          "There was an error logging in with your credentials please try again later",
+        variant: "destructive",
+      });
+    } finally {
+      setIsCredentials(false);
+    }
+  };
+  return (
+    <div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Icons.logo className="mx-auto h-10 w-auto" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <Button
+              type="button"
+              className="bg-black w-full"
+              onClick={async () => {
+                try {
+                  setIsGoogle(true);
+                  await signIn("google");
+                } catch (error) {
+                  toast({
+                    title: "Error",
+                    description: "There was an error logging in with Google",
+                    variant: "destructive",
+                  });
+                } finally {
+                  setIsGoogle(false);
+                }
+              }}
+              disabled={isGoogle}
+            >
+              {isGoogle ? (
+                <Loader2 className="mr-4 h-4 w-4 animate-spin" />
               ) : null}
-              <div>
-                <Button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
-                >
-                  Sign in
-                </Button>
+              <Icons.google className="mr-2 h-4 w-4" />
+              Continue with Google
+            </Button>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t"></span>
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="grainy px-2 text-muted-foreground font-semibold">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div>
+              <Label htmlFor="email"> Email address</Label>
+              <div className="mt-2">
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="block text-sm font-medium leading-6"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <Label htmlFor="password"> Password</Label>
+                <div className="text-sm">
+                  <a
+                    href="#"
+                    className="font-semibold text-blue-600 hover:text-blue-500"
+                  >
+                    Forgot password?
+                  </a>
+                </div>
               </div>
               <div className="mt-2">
-                <Label>
-                  {" "}
-                  No account?{" "}
-                  <Link href="/registration" className="text-blue-500">
-                    Sign Up
-                  </Link>
-                </Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
               </div>
-            </form>
-          </div>
+            </div>
+            {error ? (
+              <div>
+                <div className="flex items-center justify-between">
+                  <Label className="text-red-600">
+                    Your email or password were incorrect.
+                  </Label>
+                </div>
+              </div>
+            ) : null}
+            <div>
+              <Button
+                type="submit"
+                className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+                disabled={isCredentials}
+              >
+                {isCredentials ? (
+                  <Loader2 className="mr-4 h-4 w-4 animate-spin" />
+                ) : null}
+                Sign in
+              </Button>
+            </div>
+            <div className="mt-2">
+              <Label>
+                {" "}
+                No account?{" "}
+                <Link href="/registration" className="text-blue-500">
+                  Sign Up
+                </Link>
+              </Label>
+            </div>
+          </form>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- components/Registration.tsx
+++ components/Registration.tsx
@@ -10,136 +10,156 @@ import { useState } from "react";
 import { toast } from "@/components/ui/use-toast";
 import { useRouter } from "next/navigation";
 import { signIn } from "next-auth/react";
+import { Loader2 } from "lucide-react";
 
- const Registration = () => {
-    const [names, setNames] = useState<string>("");
-    const [email, setEmail] = useState<string>("");
-    const [password, setPassword] = useState<string>("");
-    const router = useRouter();
-  
-    const { mutate: register } = trpc.createUser.useMutation({
-      onError: () => {
-        return toast({
-          title: "There was problem creating an account",
-          description:
-            "The email has already been taken. Please try a different email.",
-          variant: "destructive",
-        });
-      },
-      onSuccess: () => {
-        router.push("/login");
-        return toast({
-          title: "Successfull",
-          description: "Account has been registered!",
-        });
-      },
-    });
-  
-    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-      event.preventDefault();
-      register({
-        names: names,
-        email: email,
-        password: password,
+const Registration = () => {
+  const [names, setNames] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const router = useRouter();
+  const [isGoogle, setIsGoogle] = useState<boolean>(false);
+  const { mutate: register, isLoading } = trpc.createUser.useMutation({
+    onError: () => {
+      return toast({
+        title: "There was problem creating an account",
+        description:
+          "The email has already been taken. Please try a different email.",
+        variant: "destructive",
       });
-    };
-  
-    return (
-      <div>
-        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <Icons.logo className="mx-auto h-10 w-auto" />
-            <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-              Create your account
-            </h2>
-          </div>
-  
-          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" onSubmit={handleSubmit}>
+    },
+    onSuccess: () => {
+      router.push("/login");
+      return toast({
+        title: "Successfull",
+        description: "Account has been registered!",
+      });
+    },
+  });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    register({
+      names: names,
+      email: email,
+      password: password,
+    });
+  };
+
+  return (
+    <div>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <Icons.logo className="mx-auto h-10 w-auto" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Create your account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <Button
+              type="button"
+              onClick={async () => {
+                try {
+                  setIsGoogle(true);
+                  await signIn("google");
+                } catch (error) {
+                  toast({
+                    title: "Error",
+                    description: "There was an error logging in with Google",
+                    variant: "destructive",
+                  });
+                } finally {
+                  setIsGoogle(false);
+                }
+              }}
+              className="bg-black w-full"
+              disabled={isGoogle}
+            >
+              {isGoogle ? (
+                <Loader2 className="mr-4 h-4 w-4 animate-spin" />
+              ) : null}
+              <Icons.google className="mr-2 h-4 w-4" />
+              Continue with Google
+            </Button>
+            <div className="relative">
+              <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t"></span>
+              </div>
+              <div className="relative flex justify-center text-xs uppercase">
+                <span className="grainy px-2 text-muted-foreground font-semibold">
+                  Or continue with
+                </span>
+              </div>
+            </div>
+            <div>
+              <Label htmlFor="names">Name</Label>
+              <div className="mt-1">
+                <Input
+                  id="names"
+                  name="names"
+                  type="text"
+                  className="block text-sm font-medium leading-6"
+                  required
+                  onChange={(e) => setNames(e.target.value)}
+                />
+              </div>
+            </div>
+            <div>
+              <Label htmlFor="email"> Email address</Label>
+              <div className="mt-1">
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block text-sm font-medium leading-6"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <Label htmlFor="password"> Password</Label>
+              </div>
+              <div className="mt-1">
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div>
               <Button
-                type="button"
-                onClick={() => {
-                  signIn("google");
-                }}
-                className="bg-black w-full"
+                type="submit"
+                className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
+                disabled={isLoading}
               >
-                <Icons.google className="mr-2 h-4 w-4" />
-                Continue with Google
+                {isLoading ? (
+                  <Loader2 className="mr-4 h-4 w-4 animate-spin" />
+                ) : null}
+                Register
               </Button>
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <span className="w-full border-t"></span>
-                </div>
-                <div className="relative flex justify-center text-xs uppercase">
-                  <span className="grainy px-2 text-muted-foreground font-semibold">
-                    Or continue with
-                  </span>
-                </div>
-              </div>
-              <div>
-                <Label htmlFor="names">Name</Label>
-                <div className="mt-1">
-                  <Input
-                    id="names"
-                    name="names"
-                    type="text"
-                    className="block text-sm font-medium leading-6"
-                    required
-                    onChange={(e) => setNames(e.target.value)}
-                  />
-                </div>
-              </div>
-              <div>
-                <Label htmlFor="email"> Email address</Label>
-                <div className="mt-1">
-                  <Input
-                    id="email"
-                    name="email"
-                    type="email"
-                    autoComplete="email"
-                    className="block text-sm font-medium leading-6"
-                    onChange={(e) => setEmail(e.target.value)}
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <div className="flex items-center justify-between">
-                  <Label htmlFor="password"> Password</Label>
-                </div>
-                <div className="mt-1">
-                  <Input
-                    id="password"
-                    name="password"
-                    type="password"
-                    autoComplete="current-password"
-                    required
-                    onChange={(e) => setPassword(e.target.value)}
-                  />
-                </div>
-              </div>
-  
-              <div>
-                <Button
-                  type="submit"
-                  className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm font-semibold leading-6 shadow-sm  focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 "
-                >
-                  Register
-                </Button>
-              </div>
-              <div>
-                <Label>
-                  {" "}
-                  Have an account?{" "}
-                  <Link href="/login" className="text-blue-500">
-                    Sign In
-                  </Link>
-                </Label>
-              </div>
-            </form>
-          </div>
+            </div>
+            <div>
+              <Label>
+                {" "}
+                Have an account?{" "}
+                <Link href="/login" className="text-blue-500">
+                  Sign In
+                </Link>
+              </Label>
+            </div>
+          </form>
         </div>
       </div>
-    );
-}
-export default Registration
+    </div>
+  );
+};
+export default Registration;
