b1e1f8913ec6b71930a89c3b80dbedd20424457b
primostasis123 <editomoncada123@gmail.com>
2024-02-11 00:24:54 +0800

    hard coded sandbox

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -22,17 +22,17 @@ export async function getUserSubscriptionPlan() {
     };
   }
 
-  // const response = await fetch(
-  //   `${process.env.PAYPAL_URL_API}/v1/oauth2/token`,
-  //   {
-  //     method: "POST",
-  //     headers: {
-  //       "Content-Type": "application/x-www-form-urlencoded",
-  //       Authorization: `Basic ${btoa(`${clientId}:${clientSecret}`)}`,
-  //     },
-  //     body: "grant_type=client_credentials",
-  //   }
-  // );
+  const response = await fetch(
+    `https://api.sandbox.paypal.com/v1/oauth2/token`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        Authorization: `Basic ${btoa(`AbEaJSSvLTzjcZJylDGlEQ5fQgUHzfkjlp4B9aGUgGEQT5T7qAVVBSDoY2GFmx_2Ae8QZzlQ1R8F9eaN:EDOyZe3RN7TSlyVAlkIixLsS-G8cu5zTpGGonwl3amXM0Q_EdHglnrwVxkiO6bjKdMsJgq-m1I0nXgj5`)}`,
+      },
+      body: "grant_type=client_credentials",
+    }
+  );
 
 
   return {
