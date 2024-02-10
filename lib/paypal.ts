86c4690392f7d5a017b84c0de986b3e0aef8f8c5
primostasis123 <editomoncada123@gmail.com>
2024-02-11 00:52:55 +0800

    Update paypal.ts

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -37,57 +37,45 @@ export async function getUserSubscriptionPlan() {
     }
   );
 
+  const data = await response.json();
+  const accessToken = data.access_token;
+  const subscriptionId = dbUser.subscriptionId;
 
-  return {
-    subscriptionId : null,
-    plan: "Free",
-    isSubscribed: false,
-    isCanceled: false,
-    periodEnd: null,
-    token: null,
-  };
+  const subscriptionResponse = await fetch(
+    `${url}/v1/billing/subscriptions/${subscriptionId}`,
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
+  );
 
+  const subscriptionData = await subscriptionResponse.json();
+  // Get the current date
+  const periodEnd = new Date(subscriptionData.start_time);
+  periodEnd.setMonth(periodEnd.getMonth() + 1);
+  const currentDate = new Date()
 
-
-  // const data = await response.json();
-  // const accessToken = data.access_token;
-  // const subscriptionId = dbUser.subscriptionId;
-
-  // const subscriptionResponse = await fetch(
-  //   `${process.env.PAYPAL_URL_API}/v1/billing/subscriptions/${subscriptionId}`,
-  //   {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: `Bearer ${accessToken}`,
-  //     },
-  //   }
-  // );
-
-  // const subscriptionData = await subscriptionResponse.json();
-  // // Get the current date
-  // const periodEnd = new Date(subscriptionData.start_time);
-  // periodEnd.setMonth(periodEnd.getMonth() + 1);
-  // const currentDate = new Date()
-
-  // if (periodEnd <= currentDate) {
-  //   if (subscriptionData.status === "CANCELLED") {
-  //     return {
-  //       subscriptionId : null,
-  //       plan: "Free",
-  //       isSubscribed: false,
-  //       isCanceled: false,
-  //       periodEnd: null,
-  //       token: null,
-  //     };
-  //   }
-  // }
+  if (periodEnd <= currentDate) {
+    if (subscriptionData.status === "CANCELLED") {
+      return {
+        subscriptionId : null,
+        plan: "Free",
+        isSubscribed: false,
+        isCanceled: false,
+        periodEnd: null,
+        token: null,
+      };
+    }
+  }
   
-  // return {
-  //   subscriptionId : subscriptionId,
-  //   plan: subscriptionData.plan_id === process.env.PRO_PLAN ? "Pro" : "Basic",
-  //   isSubscribed: true,
-  //   isCanceled: subscriptionData.status === "ACTIVE" ? false : true,
-  //   periodEnd: subscriptionData.status === "ACTIVE" ? subscriptionData.billing_info.next_billing_time : periodEnd,
-  //   token : data.access_token
-  // };
+  return {
+    subscriptionId : subscriptionId,
+    plan: subscriptionData.plan_id === process.env.PRO_PLAN ? "Pro" : "Basic",
+    isSubscribed: true,
+    isCanceled: subscriptionData.status === "ACTIVE" ? false : true,
+    periodEnd: subscriptionData.status === "ACTIVE" ? subscriptionData.billing_info.next_billing_time : periodEnd,
+    token : data.access_token
+  };
 }
