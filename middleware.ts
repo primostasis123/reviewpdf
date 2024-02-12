b9a90634e3e0e89fd30504d2f4897418aef370da
primostasis123 <editomoncada123@gmail.com>
2024-02-12 17:48:30 +0800

    middleware and schema

--- middleware.ts
+++ middleware.ts
@@ -4,7 +4,7 @@ import { NextRequest } from "next/server";
 // This function can be marked `async` if using `await` inside
 export async  function middleware(request: NextRequest) {
     const path = request.nextUrl.pathname;
-    const session = !!request.cookies.get("authjs.session-token")
+    const session = !!request.cookies.get("authjs.session-token") || !!request.cookies.get("__Secure-authjs.session-token")
     if (!session) {
         return NextResponse.redirect(new URL("/", request.url));
     }
